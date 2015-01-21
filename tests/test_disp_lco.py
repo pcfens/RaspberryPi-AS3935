9dff92ac75fae301890631a884bdffaa28976250
Phil Fenstermacher <pcfens@wm.edu>
2015-01-21 09:52:09 -0500

    Add test functions to getters that return booleans

--- tests/test_disp_lco.py
+++ tests/test_disp_lco.py
@@ -1,3 +1,8 @@
 def test_set_disp_lco(as3935):
     as3935.set_disp_lco(True)
     assert (as3935.registers[0x08] & 0x80) == 0x80
+
+
+def test_get_disp_lco(as3935):
+    disp_lco = as3935.get_disp_lco()
+    assert disp_lco is True
--- tests/test_disturbers.py
+++ tests/test_disturbers.py
@@ -1,3 +1,8 @@
 def test_set_mask_disturber(as3935):
     as3935.set_mask_disturber(True)
     assert (as3935.registers[0x03] & 0x20) == 0x20
+
+
+def test_get_mask_disturber(as3935):
+    mask_disturber = as3935.get_mask_disturber()
+    assert mask_disturber is True
