870783bc45a5c3eb1040f4a814fb2b743b2017f4
Phil Fenstermacher <phillip.fenstermacher@gmail.com>
2015-01-20 21:47:11 -0500

    Update to version 0.0.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='RPi_AS3935',
-    version='0.0.8',
+    version='0.0.9',
     description='Raspberry Pi <-I2C-> AS3935 lightning sensor communication library',
     author='Phil Fenstermacher',
     author_email='phillip.fenstermacher@gmail.com',
